1ce49a820b121261d4df860cb8400d6ce5a8b6b5
Tracy <ttram1@my.bcit.ca>
2025-10-16 13:09:45 -0700

    finished stables lab 2

--- stables.js
+++ stables.js
@@ -69,9 +69,9 @@ let lateFeeMessage =
     monthlyBoardingFee +
     " and the 20% late fee.";
 
-//------------------------- First day (Lab #2, Week 4) -------------------------//
+// //------------------------- First day (Lab #2, Week 4) -------------------------//
 
-//IN-CLASS UPDATED CODE//
+// //IN-CLASS UPDATED CODE//
 let horses = {
     Meera: {
         name: "Meera",
@@ -124,73 +124,35 @@ horses.lourdes.isHungry = false;
 
 //------------------------- Growing our business (Lab #2, Week 6) -------------------------//
 
-// Create a variable that stores your total number of stalls.
-let stableStalls = 6;
-// Create a function that logs out how many stalls are available, given how many horses you have in your stable.
-function availableStalls(stableStalls, occupiedStalls) {
-    let unoccupiedStalls = stableStalls - occupiedStalls;
-    console.log("There are currently " + unoccupiedStalls + " available.");
+const NUMBER_OF_STALLS = 10;
+function availableStalls(numberOfStalls, numberOfHorses) {
+    // 1. figure out how many stalls are available
+    // 2. log that info out
+    let numberOfAvailStalls = numberOfStalls - numberOfHorses;
+    console.log("There are " + numberOfAvailStalls + " stalls available");
 }
-
-// Create a function that logs out how much will an individual horse will owe if rent is
-// paid late. Invoke the function.
-
-function lateFeeStatement(horse, rent, lateFee) {
-    let rentTotal = rent + lateFee;
+function latePaymentNotice(horse) {
     console.log(
-        horse.name +
-            ", you haven't paid your monthly fee yet. You know owe " +
-            rentTotal
+        " Your payment for " +
+            horse.name +
+            " is late! You owe $" +
+            horse.rent * 0.2 +
+            ", plus the monthly fee of $" +
+            horse.rent +
+            "."
     );
 }
 
-// Create and invoke a function that returns (not logs) the nickname of a chosen horse. Log out
-// the return value outside of the function.
-function getHorseNickname(horses) {
-    return horses.nickname;
+function gimmeThatNickName(horse) {
+    return (
+        " This is " +
+        horse.name +
+        ", their friends like to call them " +
+        horse.nickname +
+        "!"
+    );
 }
-
-availableStalls(6, 4);
-lateFeeStatement(horses.Meera, monthlyBoardingFee, lateFee);
-console.log(getHorseNickname(horses.Meera));
-
-// //Week 6 Funtion examples vv
-// function printHorseAge(horseAge, horseName) {
-//     console.log(
-//         horseName +
-//             " is " +
-//             horseAge +
-//             " years old! Next year, they will be " +
-//             (horseAge + 1) +
-//             " years old!"
-//     );
-// }
-
-// printHorseAge(12, "Khai");
-// printHorseAge(horses.Meera.age, horses.Meera.name);
-
-// let changer = " Hello world";
-// changer = " BYEEEEEEEEEEEEEE world";
-
-// const MONTHLY_RENT = 200;
-// function payMe(rent = MONTHLY_RENT) {
-//     console.log("You owe me " + rent + " dollars.");
-// }
-
-// payMe(1000);
-// payMe(200);
-// payMe();
-
-// function payMe(rent = MONTHLY_RENT) {
-//     let message = "You owe me " + rent + " dollars.";
-//     return message;
-// }
-// function payMe(rent = MONTHLY_RENT) {
-//     return "You owe me " + rent + " dollars.";
-// }
-// console.log(payMe());
-
-// //function payMeLateFees(lateFees) {
-//     let amount = "You owe me " + lateFees + " dollars.";
-//     return amount;
-// }
+availableStalls(NUMBER_OF_STALLS, 5);
+latePaymentNotice(horses.Jihee);
+latePaymentNotice(horses.lourdes);
+console.log(gimmeThatNickName(horses.Sofie, horses.Sofie.nickname));
